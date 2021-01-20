bfc2b497857a32a1ee5b99bce378cce44ef0f2ed
hossi-py <68405946+hossi-py@users.noreply.github.com>
2021-01-20 15:18:03 +0900

    [Front] 상단네비게이션 바 구현

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/main/home">홈</router-link> |
+      <TopNavi/>
+      <!-- <router-link to="/main/home">홈</router-link> |
       <router-link to="/main/search">검색</router-link> |
       <router-link to="/main/makemind">생성</router-link> |
       <router-link to="/main/activity">활동</router-link> |
-      <router-link to="/main/mylibrary">내 책장</router-link>
+      <router-link to="/main/mylibrary">내 책장</router-link> -->
     </div>
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
+import TopNavi from '@/components/navi/TopNavi.vue'
 
+export default {
+  name: 'Main',
+  components: {
+    TopNavi
+  }
 }
 </script>
 
