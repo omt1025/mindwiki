4dd34cfc2f10c970baa62c4784737ba5a5b9d69e
giga1615 <45623764+giga1615@users.noreply.github.com>
2021-01-21 13:30:53 +0900

    [Front] 로그인UI변경
    Login2.vue추가.
    기존 로그인은 참고한 후 삭제할예정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,55 +1,53 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-// import store from '@/store';
+// import store from '@/store;
+// User
+import Login from '@/views/user/Login2.vue';
+// Main
 import Home from '../views/Home.vue';
 import Main from '../views/Main.vue';
-// User
-import Login from '@/views/user/Login.vue';
-// Main
-import Home from '../views/Home.vue'
-import Main from '../views/Main.vue'
-import Search from '../views/main/Search.vue'
-import MakeMind from '../views/main/MakeMind.vue'
-import Activity from '../views/main/Activity.vue'
-import MyLibrary from '../views/main/MyLibrary.vue'
+import Search from '../views/main/Search.vue';
+import MakeMind from '../views/main/MakeMind.vue';
+import Activity from '../views/main/Activity.vue';
+import MyLibrary from '../views/main/MyLibrary.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  { 
-    path: '/', 
-    name: 'Home', 
-    component: Home 
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
   },
-  { 
-    path: '/login', 
-    name: 'Login', 
-    component: Login 
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
   },
   {
     path: '/main',
     name: 'Main',
-    component: Main
+    component: Main,
   },
   {
     path: '/main/search',
     name: 'Search',
-    component: Search
+    component: Search,
   },
   {
     path: '/main/makemind',
     name: 'MakeMind',
-    component: MakeMind
+    component: MakeMind,
   },
   {
     path: '/main/activity',
     name: 'Activity',
-    component: Activity
+    component: Activity,
   },
   {
     path: '/main/mylibrary',
     name: 'MyLibrary',
-    component: MyLibrary
+    component: MyLibrary,
   },
 ];
 
