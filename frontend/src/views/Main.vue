3d88f8c39448d198c6f79b8e2d5aa9bec1b725c0
omt1025 <omt1025@gmail.com>
2021-01-21 11:18:57 +0900

    메인페이지 탭

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -3,29 +3,79 @@
     <TopNavi/>
     <BottomNavi/>
     <v-tabs
-    fixed-tabs
-    background-color="primary"
-    dark
-  >
-    <v-tab
-      v-for="item in items"
-      :key="item.tab"
+      v-model="tabs"
+      fixed-tabs
+      background-color="primary"
+      dark
     >
-      {{ item.tab }}
-    </v-tab>
-  </v-tabs>
+      <v-tab
+        v-for="item in items"
+        :key="item.tab"
+      >
+        {{ item.tab }}
+      </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tabs">
+      <Card/>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <Card/>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-title class="headline">
+            An awesome title
+          </v-card-title>
+          <v-card-text>
+            <p>
+              Duis lobortis massa imperdiet quam. Donec vitae orci sed dolor rutrum auctor. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Praesent congue erat at massa.
+            </p>
+
+            <p>
+              Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Etiam sit amet orci eget eros faucibus tincidunt. Donec sodales sagittis magna.
+            </p>
+
+            <p class="mb-0">
+              Ut leo. Suspendisse potenti. Duis vel nibh at velit scelerisque suscipit. Fusce pharetra convallis urna.
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-title class="headline">
+            An even better title
+          </v-card-title>
+          <v-card-text>
+            <p>
+              Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Sed hendrerit. Maecenas malesuada. Vestibulum ullamcorper mauris at ligula. Proin faucibus arcu quis ante.
+            </p>
+
+            <p class="mb-0">
+              Etiam vitae tortor. Curabitur ullamcorper ultricies nisi. Sed magna purus, fermentum eu, tincidunt eu, varius ut, felis. Aliquam lobortis. Suspendisse potenti.
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
   </v-app>
 </template>
 
 <script>
 import TopNavi from '@/components/navi/TopNavi.vue'
 import BottomNavi from '@/components/navi/BottomNavi.vue'
+import Card from '@/components/common/Card.vue'
 
 export default {
   name: 'Main',
   components: {
     TopNavi,
-    BottomNavi
+    BottomNavi,
+    Card
   },
   data () {
     return {
