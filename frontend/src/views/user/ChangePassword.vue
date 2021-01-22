c44fa05f0693255a47c1d3545fb5cacf6538f435
hossi-py <68405946+hossi-py@users.noreply.github.com>
2021-01-22 16:57:37 +0900

    [Front] 비밀번호 변경 조건 구현

--- frontend/src/views/user/ChangePassword.vue
+++ frontend/src/views/user/ChangePassword.vue
@@ -1,47 +1,87 @@
 <template>
   <div class="passwordChange_back">
-    <div class="limiter">
       <div class="container-login100">
         <div class="wrap-login100 p-l-55 p-r-55 p-t-50 p-b-35">
-          <v-form class="login100-form validate-form" ref="form" v-model="valid">
+
             <span class="login100-form-title p-b-49">
               비밀번호 변경
             </span>
-              <v-text-field
-                label="Password"
-                v-model="password"
-                :rules="[rules.passwordRules]"
-                type="password"
-                required>
-              </v-text-field>
-              <v-text-field
-                label="Confirm Password"
-                v-model="confirmPassword"
-                :rules="[rules.confirmPasswordRules, passwordConfirmationRule]"
-                @update:error="checkPassword"
-                type="password"
-                required>
-              </v-text-field>
-          </v-form>
+
+            <v-text-field
+              label="변경할 비밀번호"
+              v-model="password"
+              :rules="[rules.passwordRules]"
+              type="password"
+              prepend-icon="fa fa-lock"
+              required>
+            </v-text-field>
+            <v-text-field
+              label="비밀번호 확인"
+              v-model="confirmPassword"
+              :rules="[rules.confirmPasswordRules, passwordConfirmationRule]"
+              @update:error="checkPassword"
+              type="password"
+              prepend-icon="fa fa-lock"
+              required>
+            </v-text-field>
+
+            <br>
+
+            <div class="wrap-login100-form-btn">
+              <div class="login100-form-bgbtn"></div>
+              <button 
+                class="login100-form-btn"
+                @click="changePassword"
+                :disabled="!isSubmit"
+                :class="{disabled : !isSubmit}"
+              >변경하기</button>
+            </div>
+
+            <br>
+
+            <div class="wrap-login100-form-btn">
+              <div class="login100-form-bgbtn"></div>
+              <button 
+                class="login100-form-btn"
+                @click="backPage"
+              >뒤로가기</button>
+            </div>
+
         </div>
       </div>
-    </div>
   </div>
 </template>
 
 <script>
 
   export default {
+    created() {
+      this.component = this;
+    },
     data() {
       return {
         password: "",
         confirmPassword: "",
         validPassword: "",
         rules: {
-          passwordRules: v => !!v || "Password is required",
-          confirmPasswordRules: v => !!v || "Password is required"
+          passwordRules: v => {
+            const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#%&])(?=.{8,})/;
+            return (
+              pattern.test(v) ||
+              "영문자(대문자 최소 1개 포함), 숫자, 특수문자($, ^, * 제외) 8자리 이상"
+            )
+          } ,
+          confirmPasswordRules: v => !!v || "비밀번호를 입력해주세요."
         }
       };
+    },
+    watch: {
+      password: function() {
+        this.passwordRules()
+      },
+      confirmPassword: function() {
+        this.confirmPasswordRules()
+      }
     },
     methods: {
       checkPassword(invalid) { 
@@ -51,11 +91,31 @@
         } else {
           this.validPassword = true;
         }
+      let isSubmit = true
+      Object.values(this.error).map(v => {
+        if (v) isSubmit = false;
+      });
+      this.isSubmit = isSubmit;
       },
+      backPage: function () {
+        this.$router.push('/main/menu')
+      },
+      changePassword() {
+        if (this.isSubmit) {
+          // let { password, confirmPassword, validPassword } = this;
+          // let data = {
+          //   password,
+          //   confirmPassword,
+          //   validPassword,
+          // };
+          this.isSubmit = false;
+          this.$router.push('/main/menu')
+        }
+      }
     },
     computed: {
       passwordConfirmationRule() {
-        return () => (this.password === this.confirmPassword) || 'Password must match'
+        return () => (this.password === this.confirmPassword) || '비밀번호가 일치하지 않습니다.'
     },
   }
 };
