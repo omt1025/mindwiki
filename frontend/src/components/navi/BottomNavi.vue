f9a5eae1641359b4edc11a27cb5d78b1a1f1fe9a
giga1615 <45623764+giga1615@users.noreply.github.com>
2021-01-25 15:22:20 +0900

    [Front] Activity 임시 페이지

--- frontend/src/components/navi/BottomNavi.vue
+++ frontend/src/components/navi/BottomNavi.vue
@@ -1,14 +1,6 @@
 <template>
   <v-app>
-    <v-bottom-navigation
-      v-model="value"
-      :background-color="color"
-      dark
-      shift
-      fixed
-      justify="space"
-    >
-      
+    <v-bottom-navigation v-model="value" :background-color="color" dark shift fixed justify="space">
       <v-btn>
         <span>홈</span>
 
@@ -27,9 +19,8 @@
         <v-icon>mdi-head-plus-outline</v-icon>
       </v-btn>
 
-      <v-btn>
+      <v-btn to="/main/activity">
         <span>활동</span>
-
         <v-icon>mdi-bell</v-icon>
       </v-btn>
 
@@ -37,28 +28,33 @@
         <span>내 책장</span>
 
         <v-icon>mdi-bookshelf</v-icon>
-      </v-btn>  
-    
+      </v-btn>
     </v-bottom-navigation>
   </v-app>
 </template>
 
 <script>
-  export default {
-    name: 'BottomNavi',
-    data: () => ({ value: 0 }),
+export default {
+  name: 'BottomNavi',
+  data: () => ({ value: 0 }),
 
-    computed: {
-      color () {
-        switch (this.value) {
-          case 0: return 'primary'
-          case 1: return 'primary'
-          case 2: return 'primary'
-          case 3: return 'primary'
-          case 4: return 'primary'
-          default: return 'blue-grey'
-        }
-      },
+  computed: {
+    color() {
+      switch (this.value) {
+        case 0:
+          return 'primary';
+        case 1:
+          return 'primary';
+        case 2:
+          return 'primary';
+        case 3:
+          return 'primary';
+        case 4:
+          return 'primary';
+        default:
+          return 'blue-grey';
+      }
     },
-  }
+  },
+};
 </script>
--- frontend/src/views/main/Activity.vue
+++ frontend/src/views/main/Activity.vue
@@ -1,23 +1,27 @@
 <template>
   <v-app>
-    <ActivityNavi/>
-    <BottomNavi/>
+    <!-- <ActivityNavi/> -->
+    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+      <!-- Navbar content -->
+      <a class="navbar-brand" href="#"
+        ><img src="@/assets/images/user/mindwiki_logo.png" height="27px"
+      /></a>
+    </nav>
+    <BottomNavi />
   </v-app>
 </template>
 
 <script>
-import ActivityNavi from '@/components/navi/ActivityNavi.vue'
-import BottomNavi from '@/components/navi/BottomNavi.vue'
+// import ActivityNavi from '@/components/navi/ActivityNavi.vue';
+import BottomNavi from '@/components/navi/BottomNavi.vue';
 
 export default {
   name: 'Search',
   components: {
-    ActivityNavi,
+    // ActivityNavi,
     BottomNavi,
   },
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
