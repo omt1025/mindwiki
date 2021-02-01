6474d1b446ac86aaeeb5118444da8cb4bba1afe9
omt1025 <omt1025@naver.com>
2021-02-01 16:06:59 +0900

    [Front] 마인드맵 생성 페이지

--- frontend/src/views/MindMapDetail.vue
+++ frontend/src/views/MindMapDetail.vue
@@ -3,16 +3,128 @@
     <v-app-bar id="navi_shadow" shift fixed color="white" light>
       <v-icon @click="backPage">mdi-keyboard-backspace</v-icon>
     </v-app-bar>
-    <div>
-      <mindmap :nodes="nodes" :connections="connections" :editable="true" />
-      <v-btn
-        id="btn"
-        depressed
-        color="primary"
+    <mindmap :nodes="nodes" :connections="connections" :editable="true" />
+  
+    <v-row justify="center">
+      <v-dialog
+        v-model="dialog"
+        persistent
+        max-width="600px"
       >
-        노드생성
-      </v-btn>
-    </div>
+        <template v-slot:activator="{ on, attrs }">
+          <v-card-text style="height: 100px; position: relative">
+            <v-fab-transition>
+              <v-btn
+                color="purple"
+                dark
+                absolute
+                top
+                right
+                fab
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>mdi-plus</v-icon>
+              </v-btn>
+            </v-fab-transition>
+          </v-card-text>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="headline">User Profile</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Legal first name*"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Legal middle name"
+                    hint="example of helper text only on focus"
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Legal last name*"
+                    hint="example of persistent helper text"
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Email*"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Password*"
+                    type="password"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                >
+                  <v-select
+                    :items="['0-17', '18-29', '30-54', '54+']"
+                    label="Age*"
+                    required
+                  ></v-select>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                >
+                  <v-autocomplete
+                    :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                    label="Interests"
+                    multiple
+                  ></v-autocomplete>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="dialog = false"
+            >
+              Close
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="dialog = false"
+            >
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
   </div>
 </template>
 
@@ -22,29 +134,27 @@ import map from '../map.js';
 export default {
   name: 'app',
   components: {},
-  data() {
-    return {
-      ...map,
-      newnode: {
-        text: '',
-        url: '',
-        category: '',
-        color: '',
-      },
-      newconnection: {
-        source: '',
-        target: '',
-      }
-      };
-  },
+  data: () => ({
+    dialog: false,
+    ...map,
+    newnode: {
+      text: '',
+      url: '',
+      category: '',
+      color: '',
+      fx: '',
+      fy: '',
+    },
+    newconnection: {
+      source: '',
+      target: '',
+    }
+  }),
 };
 </script>
 
 <style scoped>
 #app {
   background-color: #f1f5f8;
-}
-#btn {
-  z-index: 1;
 }
 </style>
