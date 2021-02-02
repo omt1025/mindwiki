c25da6f1bceb553e468a9a5e22b57d29c16b5f17
giga1615 <45623764+giga1615@users.noreply.github.com>
2021-02-03 00:59:59 +0900

    [Front] 안쓰는 파일(css, js) 제거

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,14 +13,11 @@ export default {
 </script>
 
 <style lang="scss">
-@import './assets/vendor/bootstrap/css/bootstrap.min.css';
-@import './assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css';
-@import './assets/fonts/iconic/css/material-design-iconic-font.min.css';
-@import './assets/vendor/animate/animate.css';
-@import './assets/vendor/css-hamburgers/hamburgers.min.css';
-@import './assets/vendor/animsition/css/animsition.min.css';
-@import './assets/vendor/select2/select2.min.css';
-@import './assets/vendor/daterangepicker/daterangepicker.css';
+@import './assets/vendor/bootstrap/css/bootstrap.min.css'; // bootstrap
+@import './assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css'; // fontawesome 아이콘
+@import './assets/fonts/iconic/css/material-design-iconic-font.min.css'; // iconic
+
+// 공통 적용되는 css파일
 @import './assets/css/util.css';
 @import './assets/css/main.css';
 
