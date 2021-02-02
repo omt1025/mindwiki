4d3dd17a36f0990192c15800c0687e1be636457b
hossi-py <68405946+hossi-py@users.noreply.github.com>
2021-02-02 09:59:15 +0900

    [Front] 마인드맵 상세 에러 수정

--- frontend/src/views/MindMapDetail.vue
+++ frontend/src/views/MindMapDetail.vue
@@ -1,8 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar id="navi_shadow" shift fixed color="white" light>
-      <v-icon @click="backPage">mdi-keyboard-backspace</v-icon>
-    </v-app-bar>
+    <v-container>
     <mindmap :nodes="nodes" :connections="connections" :editable="true" />
     <v-menu
       v-model="menu"
@@ -60,107 +58,55 @@
                 solo
                 id="combobox"
               >
-                <v-icon>mdi-plus</v-icon>
+                <template v-slot:selection="{ attrs, item, select, selected }">
+                  <v-chip
+                    v-bind="attrs"
+                    :input-value="selected"
+                    close
+                    class="ma-2"
+                    color="orange"
+                    text-color="white"
+                    @click="select"
+                    @click:close="remove(item)"
+                  >
+                    <strong class="test">{{ item }}</strong
+                    >&nbsp;
+                  </v-chip>
+                </template>
               </v-combobox>
+
+              <v-col cols="12">
+                <v-text-field
+                  label="설명"
+                  single-line
+                ></v-text-field>
+              </v-col>
             </v-row>
-          </v-card-text>
-        </template>
-        <v-card>
-          <v-card-title>
-            <span class="headline">User Profile</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col
-                  cols="12"
-                  sm="6"
-                  md="4"
-                >
-                  <v-text-field
-                    label="Legal first name*"
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col
-                  cols="12"
-                  sm="6"
-                  md="4"
-                >
-                  <v-text-field
-                    label="Legal middle name"
-                    hint="example of helper text only on focus"
-                  ></v-text-field>
-                </v-col>
-                <v-col
-                  cols="12"
-                  sm="6"
-                  md="4"
-                >
-                  <v-text-field
-                    label="Legal last name*"
-                    hint="example of persistent helper text"
-                    persistent-hint
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field
-                    label="Email*"
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field
-                    label="Password*"
-                    type="password"
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col
-                  cols="12"
-                  sm="6"
-                >
-                  <v-select
-                    :items="['0-17', '18-29', '30-54', '54+']"
-                    label="Age*"
-                    required
-                  ></v-select>
-                </v-col>
-                <v-col
-                  cols="12"
-                  sm="6"
-                >
-                  <v-autocomplete
-                    :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                    label="Interests"
-                    multiple
-                  ></v-autocomplete>
-                </v-col>
-              </v-row>
-            </v-container>
-            <small>*indicates required field</small>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              color="blue darken-1"
-              text
-              @click="dialog = false"
-            >
-              Close
-            </v-btn>
-            <v-btn
-              color="blue darken-1"
-              text
-              @click="dialog = false"
-            >
-              Save
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-row>
+          </v-container>
+            <small>container 크기 줄여야함</small>
+        </v-card-text>
+        
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            text
+            @click="menu = false"
+          >
+            Cancel
+          </v-btn>
+
+          <v-btn
+            text
+          >
+            OK
+          </v-btn>
+
+        </v-card-actions>
+      </v-card>
+    </v-menu>
+    </v-container>
   </v-app>
 </template>
 
@@ -218,4 +164,3 @@ export default {
   max-height: 500px;
 }
 </style>
-
