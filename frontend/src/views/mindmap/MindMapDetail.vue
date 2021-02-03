79b4870c55339708d18fe2fb279a00238a08abf0
omt1025 <omt1025@naver.com>
2021-02-03 11:57:29 +0900

    [Front] 마이 마인드 리스트

--- frontend/src/views/mindmap/MindMapDetail.vue
+++ frontend/src/views/mindmap/MindMapDetail.vue
@@ -1,118 +1,104 @@
 <template>
   <v-app>
     <v-container>
-    <!-- <mindmap :nodes="nodes" :connections="connections" :editable="true" /> -->
-    <v-menu
-      v-model="menu"
-      :close-on-content-click="false"
-      :nudge-width="200"
-      offset-x
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="purple"
-          dark
-          v-bind="attrs"
-          v-on="on"
-          fab
-          small
-        >
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
-      </template>
+      <!-- <mindmap :nodes="nodes" :connections="connections" :editable="true" /> -->
+      <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        :nudge-width="200"
+        offset-x
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="purple" dark v-bind="attrs" v-on="on" fab small>
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </template>
 
-      <v-card>
-        <v-card-title>
-          <span class="headline">마인드맵 생성</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12">
-                <v-text-field
-                  label="제목"
-                  required
-                  @keypress.enter="checkHandler"
-                  type="text"
-                ></v-text-field>
-              </v-col>
+        <v-card>
+          <v-card-title>
+            <span class="headline">마인드맵 생성</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    label="제목"
+                    required
+                    @keypress.enter="checkHandler"
+                    type="text"
+                  ></v-text-field>
+                </v-col>
 
-              <v-col cols="12">
-                <v-text-field
-                  label="주제"
-                  required
-                  id="title"
-                  ref="title"
-                  v-model="title"
-                  @keypress.enter="checkHandler"
-                  type="text"
-                ></v-text-field>
-              </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="주제"
+                    required
+                    id="title"
+                    ref="title"
+                    v-model="title"
+                    @keypress.enter="checkHandler"
+                    type="text"
+                  ></v-text-field>
+                </v-col>
 
-              <div>
-                <p class="interestTagTitle">해시태그 설정</p>
-              </div>
+                <div>
+                  <p class="interestTagTitle">해시태그 설정</p>
+                </div>
 
-              <v-combobox
-                append-icon
-                flat
-                v-model="chips"
-                chips
-                clearable
-                label="Your favorite hobbies"
-                multiple
-                no-filter
-                solo
-                id="combobox"
-              >
-                <template v-slot:selection="{ attrs, item, select, selected }">
-                  <v-chip
-                    v-bind="attrs"
-                    :input-value="selected"
-                    close
-                    class="ma-2"
-                    color="orange"
-                    text-color="white"
-                    @click="select"
-                    @click:close="remove(item)"
+                <v-combobox
+                  append-icon
+                  flat
+                  v-model="chips"
+                  chips
+                  clearable
+                  label="Your favorite hobbies"
+                  multiple
+                  no-filter
+                  solo
+                  id="combobox"
+                >
+                  <template
+                    v-slot:selection="{ attrs, item, select, selected }"
                   >
-                    <strong class="test">{{ item }}</strong
-                    >&nbsp;
-                  </v-chip>
-                </template>
-              </v-combobox>
+                    <v-chip
+                      v-bind="attrs"
+                      :input-value="selected"
+                      close
+                      class="ma-2"
+                      color="orange"
+                      text-color="white"
+                      @click="select"
+                      @click:close="remove(item)"
+                    >
+                      <strong class="test">{{ item }}</strong
+                      >&nbsp;
+                    </v-chip>
+                  </template>
+                </v-combobox>
 
-              <v-col cols="12">
-                <v-text-field
-                  label="설명"
-                  single-line
-                ></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
+                <v-col cols="12">
+                  <v-text-field label="설명" single-line></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
             <small>container 크기 줄여야함</small>
-        </v-card-text>
-        
-        <v-divider></v-divider>
+          </v-card-text>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            text
-            @click="menu = false"
-          >
-            Cancel
-          </v-btn>
+          <v-divider></v-divider>
 
-          <v-btn
-            text
-          >
-            OK
-          </v-btn>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text @click="menu = false">
+              Cancel
+            </v-btn>
 
-        </v-card-actions>
-      </v-card>
-    </v-menu>
+            <v-btn text>
+              OK
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-menu>
     </v-container>
   </v-app>
 </template>
@@ -123,35 +109,35 @@
 // import map from '../map.js';
 
 export default {
-  name: 'MindMapDetail',
+  name: "MindMapDetail",
   components: {},
   data: () => ({
     fav: true,
     menu: false,
-    chips: ['여행지', '음식', 'SNOW'],
+    chips: ["여행지", "음식", "SNOW"],
     interest: {},
     // ...map,
-    title: '',
-    url: '',
-    category: '',
-    color: '',
-    fx: '',
-    fy: '',
+    title: "",
+    url: "",
+    category: "",
+    color: "",
+    fx: "",
+    fy: "",
     newconnection: {
-      source: '',
-      target: '',
-    },  
+      source: "",
+      target: "",
+    },
   }),
   methods: {
     checkHandler() {
-      console(this.title)
-    }
+      console(this.title);
+    },
   },
   // 관심태그 삭제
   remove(item) {
     this.chips.splice(this.chips.indexOf(item), 1);
     this.chips = [...this.chips];
-  }
+  },
 };
 </script>
 
