d7ae6592bd0a888be9b471ddda3b273ef63b441d
hossi-py <68405946+hossi-py@users.noreply.github.com>
2021-02-03 10:56:49 +0900

    [Front] TopNavi css 삭제

--- frontend/src/components/main/search/MainSearch.vue
+++ frontend/src/components/main/search/MainSearch.vue
@@ -1,28 +1,11 @@
 <template>
   <v-app>
-    <!-- 프로필 검색을 눌렀을 때 -->
-    <div v-if="mainTab == 0">
-      <v-card flat>
-      </v-card>
-    </div>
-    
-    <!-- 타이틀 검색을 눌렀을 때 -->
-    <div v-if="mainTab == 1">
-      <v-card flat>
-      </v-card>
-    </div>
-    
-    <!-- 해시태그 검색을 눌렀을 때 -->
-    <div v-if="mainTab == 2">
-      <v-card flat>
-      </v-card>
-    </div>
+
+
   </v-app>
 </template>
 
 <script>
-
-
 import { mapGetters } from 'vuex';
 
 export default {
