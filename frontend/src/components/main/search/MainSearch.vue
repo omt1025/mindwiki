a422802e2aaa8e612c794ec17a88c6198f8c0af5
hossi-py <68405946+hossi-py@users.noreply.github.com>
2021-02-03 12:05:32 +0900

    [Front] 프로필 검색 1차 구현

--- frontend/src/components/main/search/MainSearch.vue
+++ frontend/src/components/main/search/MainSearch.vue
@@ -1,12 +1,37 @@
 <template>
   <v-app>
+    <v-text-field
+      type="search"
+      class="search"
+      placeholder="type here..."
+      v-model="search"
+    ></v-text-field>
 
+    <!-- <p>{{ search }}</p> -->
+    <!-- <p>{{ searchHandler }}</p> -->
 
+    <template v-for="item in searchHandler">
+      <v-list-item
+        :key="item.id"
+      >
+        <v-list-item-avatar>
+          <v-img
+            :alt="`${item.name} avatar`"
+            :src="item.avatar"
+          ></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="item.name"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </template>
   </v-app>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import { persons } from '@/data/data';
 
 export default {
   components: {
@@ -14,14 +39,42 @@ export default {
   },
   computed: {
     ...mapGetters(['mainTab']),
+    searchHandler() {
+      return this.data.filter(elem => {
+        return elem.name.toLowerCase().includes(this.search.toLowerCase());
+        // return elem.name
+
+      });
+    }
   },
   data() {
     return {
-      tab: null,
-      items: ['프로필', '타이틀', '해시태그'],
+      search: '',
+      data: []
     };
+  },
+  created() {
+    this.data = persons;
+    // console.log(this.data);
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.v-list-item__title {
+  text-align: left;
+}
+.subtitle {
+  text-align: left;
+  margin-top: 6px;
+}
+.v-list-item__content {
+  padding: 11px 0;
+}
+hr {
+  margin-top: 0.4rem;
+  margin-bottom: 0.4rem;
+  margin-left: 1.1rem;
+  margin-right: 1.1rem;
+}
+</style>
---
+++ frontend/src/data/data.js
@@ -0,0 +1,27 @@
+export const persons = [
+  {
+    id: 1,
+    name: '김정웅',
+    avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+  },
+  {
+    id: 2,
+    name: '신충현',
+    avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+  },
+  {
+    id: 3,
+    name: '오민택',
+    avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+  },
+  {
+    id: 4,
+    name: '윤지선',
+    avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+  },
+  {
+    id: 5,
+    name: '황윤호',
+    avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+  },  
+]
