0f344b2f765cb4f0b3c4c4b2cd33bd55e5ad58e2
ch <ybj0749@gmail.com>
2021-02-10 00:12:54 +0900

    [backend] comment fix

--- backend/mindwiki/src/main/java/com/mindwiki/interceptor/InterCeptorConfig.java
+++ backend/mindwiki/src/main/java/com/mindwiki/interceptor/InterCeptorConfig.java
@@ -15,7 +15,8 @@ import org.springframework.web.servlet.config.annotation.*;
 @EnableWebMvc
 public class InterCeptorConfig implements WebMvcConfigurer {
 	private static final String[] EXCLUDE_PATHS = { "/" , "/mindwiki/GoogleOAuth",
-			"/mindwiki/login","/mindwiki/oauth","/mindwiki/profile/register","/mindwiki/image","/mindwiki/image/**"
+			"/mindwiki/login","/mindwiki/oauth","/mindwiki/profile/register","/mindwiki/image","/mindwiki/image/**",
+			"/mindwiki/mind/{no}/comment/read"
 			
 			};
 	
