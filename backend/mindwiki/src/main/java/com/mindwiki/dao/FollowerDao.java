828277c06726f5b2ddb8fabc871f0174d01d95bf
ch <ybj0749@gmail.com>
2021-02-12 03:55:30 +0900

    [backend] follow_LikeTag complete

---
+++ backend/mindwiki/src/main/java/com/mindwiki/dao/FollowerDao.java
@@ -0,0 +1,23 @@
+package com.mindwiki.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.mindwiki.model.FollowerDto;
+import com.mindwiki.model.MindDto;
+
+
+
+@Repository
+public interface FollowerDao {
+
+	
+	void save(FollowerDto followInfo) throws SQLException; //생성
+	public List<FollowerDto> list(String myEmail) throws SQLException; 
+	List<MindDto> listDetail(String followerEmail) throws SQLException;
+	void deleteByFollower(FollowerDto followInfo) throws SQLException;
+	String searchNameByEmail(String followerEmail) throws SQLException;
+	
+}
