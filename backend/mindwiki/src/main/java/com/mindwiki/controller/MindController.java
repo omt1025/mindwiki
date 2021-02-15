cfef94f33d5821e664df99975a28d27c4c64af37
kjw11036 <kjw11036@naver.com>
2021-02-16 00:00:25 +0900

    [backend] node

--- backend/mindwiki/src/main/java/com/mindwiki/controller/MindController.java
+++ backend/mindwiki/src/main/java/com/mindwiki/controller/MindController.java
@@ -303,9 +303,12 @@ public class MindController {
 
 				if (file == null) {
 					mindSvc.make(mind);
+					
 					// admin,createTime(mysql에 저장되어있음)으로 검색하기 MindID를
 
 					MindID = mindSvc.getMindID(admin);// 인증된 이메일을 보냄
+					System.out.println(hashtag);
+					initNode(MindID, subject, hashtag);
 
 					StringTokenizer st = new StringTokenizer(hashtag, ",");
 					int count = 0;
@@ -325,7 +328,7 @@ public class MindController {
 				}
 
 				mindSvc.make(mind);
-
+				MindID = mindSvc.getMindID(admin);// 인증된 이메일을 보냄
 				initNode(MindID, subject, hashtag);
 
 				resultMap.put("message", "SUCCESS");
@@ -342,8 +345,8 @@ public class MindController {
 			// returnMessage="마인드 등록 실패!";
 		}
 
-		System.out.println("일단 mind controller");
 
+		System.out.println("일단 mind controller");
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
 
@@ -364,7 +367,7 @@ public class MindController {
 		StringBuilder data = null;
 		data.append("[{ label: '" + subject + "', root:true, reason:0, url:'', children: [");
 		
-		StringTokenizer st = new StringTokenizer(hashtag, "#");
+		StringTokenizer st = new StringTokenizer(hashtag, ",");
 		while(st.hasMoreTokens()) {
 			data.append("{ label: '" + st.nextToken() + "', reason:0, },");
 		}
