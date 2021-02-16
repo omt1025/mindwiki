e6fa23dc9b432abbfc3c7603eab6da6e086557db
giga1615 <45623764+giga1615@users.noreply.github.com>
2021-02-16 23:35:21 +0900

    [Front] 프로필 정보 수정

--- frontend/src/components/main/profile/ReviseProfile.vue
+++ frontend/src/components/main/profile/ReviseProfile.vue
@@ -26,13 +26,14 @@
 
         <!-- 이메일 input -->
         <span class="label-input100 p-t-20 p-b-7" style="float: left">이메일 </span>
-        <!-- <p style="font-size: 10px; float:right">(16자 이내)</p> -->
         <w-input
           class="m-t-10"
           id="useremail"
           ref="useremail"
-          v-model="user.useremail"
+          background-color="red"
+          v-model="this.profile.email"
           required
+          disabled
           @keypress.enter="checkHandler"
           type="email"
           placeholder="이메일을 입력해주세요."
@@ -45,14 +46,18 @@
           required
           id="username"
           ref="username"
-          v-model="user.username"
+          v-model="this.profile.realName"
           @keypress.enter="checkHandler"
           type="text"
+          disabled
           placeholder="이름을 입력해주세요."
         />
 
         <!-- nickName input -->
-        <span class="label-input100 p-t-20 p-b-7" style="float: left">닉네임</span>
+        <span class="label-input100 p-t-20 p-b-7" style="float: left"
+          >닉네임
+          <p style="font-size: 10px; float:right">(16자 이내)</p>
+        </span>
         <w-input
           class="m-t-10"
           required
@@ -61,11 +66,15 @@
           v-model="user.usernickName"
           @keypress.enter="checkHandler"
           type="text"
+          :maxlength="max_nickName"
           placeholder="닉네임을 입력해주세요."
         />
 
         <!-- 핸드폰번호 input -->
-        <span class="label-input100 p-t-20 p-b-7" style="float: left">핸드폰 번호</span>
+        <span class="label-input100 p-t-20 p-b-7" style="float: left"
+          >핸드폰 번호
+          <p style="font-size: 10px; float:right">('-' 제외)</p>
+        </span>
         <w-input
           class="m-t-10"
           required
@@ -74,6 +83,7 @@
           v-model="user.phoneNumber"
           @keypress.enter="checkHandler"
           type="text"
+          :maxlength="max_phone"
           placeholder="핸드폰번호를 입력해주세요."
         />
       </div>
@@ -99,10 +109,27 @@ export default {
       },
       msg: [], // 유효성검사 후, 출력할 메세지 담을 배열
       message: '', // 오류 받아 올 변수
-      // 제목, 설명 글자 수 제한
-      max_title: 16,
-      max_explanation: 145,
+      profile: '',
+      // 글자 수 제한
+      max_nickName: 16,
+      max_phone: 11,
     };
+  },
+  created() {
+    let form = new FormData();
+    form.append('jwt', this.$store.getters.getJWT);
+
+    this.$store.dispatch('myProfile', form).then(() => {
+      // 응답 결과
+      this.message = this.$store.getters.message;
+      this.profile = this.$store.getters.profile;
+      if (this.message === 'FAIL')
+        this.showAlert('세션이 만료되었습니다. 다시 로그인 해 주세요.', '프로필 수정');
+      else {
+        this.user.usernickName = this.profile.nickName;
+        this.user.phoneNumber = this.profile.phoneNumber;
+      }
+    });
   },
   methods: {
     backPage: function() {
@@ -115,27 +142,51 @@ export default {
       const previewImage = document.getElementById('preview-imgage');
       previewImage.setAttribute('src', URL.createObjectURL(event.target.files[0]));
     },
-
     // input 유효성 검사 메소드[YJS]
     checkHandler() {
       let err = true; // 에러 확인 변수
       let msg = ''; // 다이얼로그에 띄울 메세지
 
-      // 제목 input이 공백인 경우
-      !this.newmind.title && ((msg = '마인드 제목을 입력해주세요.'), (err = false));
+      // 닉네임 input이 공백인 경우
+      !this.user.usernickName && ((msg = '닉네임을 입력해주세요.'), (err = false));
 
-      // 주제 input이 공백인 경우
-      err &&
-        !this.newmind.subject &&
-        ((msg = '루트 노드가 될 주제를 입력해주세요.'), (err = false));
-
-      // 비밀번호 확인 inputdl 공백인 경우
-      err &&
-        !this.newmind.explanation &&
-        ((msg = '마인드에 대한 설명을 적어주세요.'), (err = false));
+      // 핸드폰번호 input이 공백인 경우
+      err && !this.user.phoneNumber && ((msg = '핸드폰 번호를 입력해주세요.'), (err = false));
 
       if (!err) this.showAlert(msg, '양식 오류');
-      else this.makemindmap();
+      else this.revise();
+    },
+    revise() {
+      let form = new FormData();
+      form.append('jwt', this.$store.getters.getJWT);
+      form.append('phoneNumber', this.user.phoneNumber);
+      form.append('nickName', this.user.usernickName);
+      form.append('files', this.user.files);
+
+      this.$store.dispatch('changeProfile', form).then(() => {
+        // 응답 결과
+        this.message = this.$store.getters.message;
+        if (this.message === 'FAIL')
+          this.showAlert('세션이 만료되었습니다. 다시 로그인 해 주세요.', '프로필 수정');
+        else {
+          localStorage.setItem('user-nickname', this.user.usernickName);
+          this.showAlert('수정을 완료하였습니다.', '프로필 수정 성공');
+        }
+      });
+    },
+    // 다이얼로그[YJS]
+    showAlert(msg, title) {
+      const options = { title: title, size: 'sm' };
+      this.$dialogs.alert(msg, options).then((res) => {
+        // 세션만료로 로그아웃 처리
+        if (title === '프로필 수정' && res['ok'] === true) {
+          this.$store.dispatch('setMessage', null);
+          this.$store.dispatch('LOGOUT').then(() => this.$router.replace('/').catch(() => {}));
+        } else if (title === '프로필 수정 성공' && res['ok'] === true) {
+          this.$store.dispatch('setMessage', null);
+          this.$router.push('/main');
+        }
+      });
     },
   },
 };
@@ -163,6 +214,10 @@ export default {
 .input[data-v-565818fe] {
   background-color: #00000006 !important;
   color: black;
+}
+#useremail,
+#username {
+  background-color: #f1f5f8 !important;
 }
 input:focus {
   outline: none;
